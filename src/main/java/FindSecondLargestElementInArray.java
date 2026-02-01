ef0e41f858583e104988c40291a288f7aea09cf0
jamalnihal07 <nihaljamal07@gmail.com>
2026-02-01 18:49:01 -0500

    SolveArryas

---
+++ src/main/java/FindSecondLargestElementInArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class FindSecondLargestElementInArray {
+    public static void main(String[] args) {
+        
+    }
+    public static int second(int[] arr){
+        int n = arr.length;
+        Arrays.sort(arr);
+
+    }
+}
